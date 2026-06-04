888c2209b3a61974a1c2e87248f7c3c20c6c76a2
Julia Jeng <princessmisty48@gmail.com>
2026-06-03 20:21:53 -0500

    One word fix to rouge coral tube (after an entire afternoon debugging sesh)

--- TubeCoral.cs
+++ TubeCoral.cs
@@ -12,7 +12,7 @@ public partial class TubeCoral : Node2D
 	[Signal]
 	public delegate void UnpullEventHandler();
 	
-	private static bool tubesOn = true;
+	private bool tubesOn = true;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
