f0fb4a5629ba3cb0eee0e5d3673ebf113d16c783
Julia Jeng <princessmisty48@gmail.com>
2026-06-09 20:34:10 -0500

    Added camera limits, fixed health heart ordering

---
+++ Camera2d.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+
+public partial class Camera2d : Camera2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (GetParent()?.GetParent().Name == "FirstRoom") {
+			SetLimit(Side.Left, 0);
+			SetLimit(Side.Right, 500);
+			SetLimit(Side.Top, 0);
+			SetLimit(Side.Bottom, 180);
+		}
+	}
+}
