0130c982051f55aa54835ed240969ce4c568a6a4
Julia Jeng <princessmisty48@gmail.com>
2026-06-22 17:47:25 -0500

    Room changes backwards (to do: fix respawn points to respawn on right side)

--- BoxRoom.cs
+++ BoxRoom.cs
@@ -2,7 +2,7 @@ using Godot;
 using System;
 using System.Threading.Tasks;
 
-public partial class UnderwaterTown : Node2D
+public partial class BoxRoom : Node2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -14,24 +14,14 @@ public partial class UnderwaterTown : Node2D
 	{
 		await nextRoomCheck();
 	}
-	
 	private async Task nextRoomCheck() {
 		var FaderNode = GetNode<CanvasLayer>("/root/Fader");
 		Vector2 pos = GetNode<CharacterBody2D>("UnderwaterPlayer").Position;
-		if (pos.X > 500) {
-			if (FaderNode is Fader fader) {
-				await fader.FadeIn(1.5f);
-			}
-			GetTree().ChangeSceneToFile("res://box_room.tscn");
-			if (FaderNode is Fader fade) {
-				await fade.FadeOut(1.5f);
-			}
-		}
 		if (pos.X < 0) {
 			if (FaderNode is Fader fader) {
 				await fader.FadeIn(1.5f);
 			}
-			GetTree().ChangeSceneToFile("res://first_room.tscn");
+			GetTree().ChangeSceneToFile("res://underwater_town.tscn");
 			if (FaderNode is Fader fade) {
 				await fade.FadeOut(1.5f);
 			}
